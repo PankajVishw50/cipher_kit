7ca8b0521979309d6cb20dba19d7e8cb6b18c8f2
Pankaj <pankaj.vishw.dev@gmail.com>
2023-06-21 13:27:51 +0530

    [0.0.3] cipher_kit : Patch - Fixed import Issue
    There was an issue with importing package which made
    previoud versions unusable.
    Now it is fixed.

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("app/Readme.md", "r") as file:
     long_description = file.read()
 
 setup(
     name="cipher_kit",
-    version="0.0.2",
+    version="0.0.3",
     description="Module which helps in encryption and decryption",
     author="Pankaj",
     author_email="pankaj.vishw.dev@gmail.com",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/PankajVishw50/cipher_kit",
-    py_modules=["cipher_kit"],
+    packages=find_packages("app"),
     package_dir={"": "app"},
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3 ",
-        "License :: OSI Approved :: MIT License"
+         "License :: OSI Approved :: MIT License"
 
     ],
     extra_requires={
